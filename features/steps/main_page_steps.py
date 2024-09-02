a3738a42f59556ab34075fb0a50bc29dbcb08e3c
akittles <151872598+akittles@users.noreply.github.com>
2024-09-02 12:39:06 -0400

    Update main_page_steps.py

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -6,20 +6,20 @@ from time import sleep
 @given('Open target main page')
 def open_target(context):
     context.driver.get('https://www.target.com/')
+    context.app.main_page.open()
 
 
 # search button
 @when('Search for {product}')
 def search_product(context, product):
-    context.driver.find_element(By.ID, 'search').send_keys(product)
-    context.driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
-    sleep(6)
+    context.app.header.search_product()
 
 
 @when('Click on Cart icon')
 def click_cart(context):
     context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartLink']").click()
-    #-------------DO NOT DO THIS!!
+
+    #-------------DO NOT DO THIS!!---------------
     # # find empty cart (THIS IS AN EXAMPLE OF HAVING 2 OF THE SAME STEPS)
     # @when('Click on cart icon')
     # def click_cart_icon(context):
