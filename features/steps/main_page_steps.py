447a7dcafadb6d07b37b6b54da386d1851336714
Anthony Kittles <akittlesak4@gmail.com>
2024-09-15 15:10:43 -0400

    updated to lesson 7

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -5,19 +5,18 @@ from time import sleep
 
 @given('Open target main page')
 def open_target(context):
-    context.driver.get('https://www.target.com/')
     context.app.main_page.open()
 
 
-# search button
 @when('Search for {product}')
 def search_product(context, product):
-    context.app.header.search_product()
+    print('Step:', product)
+    context.app.header.search_product(product)
 
 
 @when('Click on Cart icon')
 def click_cart(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartLink']").click()
+    context.app.header.click_cart()
 
     #-------------DO NOT DO THIS!!---------------
     # # find empty cart (THIS IS AN EXAMPLE OF HAVING 2 OF THE SAME STEPS)
@@ -38,11 +37,20 @@ def verify_header_link_amount(context, number):
     number = int(number) # turns string "6" into integer 6
     links = context.driver.find_elements(By.CSS_SELECTOR, "[id*= 'utilityNav']")
     assert len(links) == number, f'Expected {number} links, but got {len(links)}'
-    # Make sure to always assert len() for multiple elements as shown above
-    # because .find_elements() function never fails.
-    # This code with incorrect locator will always pass:
     # context.driver.find_elements(By.CSS_SELECTOR, "[id*='utilityNav2613542']")
 
+
+@then('Verify can click every link')
+def verify_click_links(context):
+    links = context.driver.find_elements(By.CSS_SELECTOR, "[id*='utilityNav']")
+
+    for i in range(len(links)):
+        # Search for links again because their internal IDs changed:
+        # to avoid Stale Element Exception
+        links = context.driver.find_elements(By.CSS_SELECTOR, "[id*='utilityNav']")
+        print(f'Clicking on link {links[i].text}')
+        links[i].click()
+        sleep(4)
 
     # ************************     NOTES        ***************************
 
