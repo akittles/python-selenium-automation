b4393f3b4a5c47a45fe6d8f3b55a594682ee979d
Anthony Kittles <akittlesak4@gmail.com>
2024-09-15 15:02:29 -0400

    updated to lesson 7

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,18 @@
+from selenium.webdriver.common.by import By
 from pages.base_page import Page
+from time import sleep
 
 
 class MainPage(Page):
+    SIDE_NAV_ADD_TO_CART_BTN = (By.CSS_SELECTOR, "[data-test='content-wrapper'] [id*='addToCartButton']")
+    SIDE_NAV_SIGN_IN_BTN = (By.CSS_SELECTOR, "[data-test='accountNav-signIn']")
 
     def open(self):
         self.open_url('https://www.target.com/')
+
+    def side_window_signin_btn(self):
+        self.driver.find_element(*self.SIDE_NAV_SIGN_IN_BTN).click()
+
+    def side_nav_click_add_to_cart(self):
+        self.driver.find_element(*self.SIDE_NAV_ADD_TO_CART_BTN).click()
+        sleep(6)
