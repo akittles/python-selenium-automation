0b2af6c7d76ce5c56f1a3694e78c8a0e05b6b951
Anthony Kittles <akittlesak4@gmail.com>
2024-09-15 15:00:39 -0400

    updated to lesson 7

--- pages/header.py
+++ pages/header.py
@@ -5,11 +5,20 @@ from pages.base_page import Page
 
 
 class Header(Page):
+    CART_BTN = (By.CSS_SELECTOR, "[data-test='@web/CartLink']")
     SEARCH_FIELD = (By.ID, 'search')
     SEARCH_BTN = (By.XPATH, "//button[@data-test='@web/Search/SearchButton']")
+    SIGN_IN_BTN = (By.CSS_SELECTOR, "[aria-label='Account, sign in']")
 
-    def search_product(self):
-        self.input_text('coffee', *self.SEARCH_FIELD)
+    def search_product(self, product):
+        print('POM layer:', product)
+        self.input_text(product, *self.SEARCH_FIELD)
         self.click(*self.SEARCH_BTN)
-        # wait for page with search results to load
-        sleep(10)
+        # wait for the page with search results to load
+        sleep(6)
+
+    def click_cart(self):
+        self.wait_and_click(*self.CART_BTN)
+
+    def click_sign_in_btn(self):
+        self.wait_and_click(*self.SIGN_IN_BTN)
