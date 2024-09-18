14014ae47f2f946f253843154d3bc872e2523d41
Anthony Kittles <akittlesak4@gmail.com>
2024-09-18 13:56:40 -0400

    Updated to homework 8

--- pages/base_page.py
+++ pages/base_page.py
@@ -10,8 +10,43 @@ class Page:
     def open_url(self, url):
         self.driver.get(url)
 
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
+
+    def find_elements(self, *locator):
+        return self.driver.find_elements(*locator)
+
+    def click(self, *locator):
+        self.driver.find_element(*locator).click()
+
     def input_text(self, text, *locator):
         self.driver.find_element(*locator).send_keys(text)
+
+    def get_current_window(self):
+        window = self.driver.current_window_handle
+        print('Current window:', window)
+        return window
+
+    def switch_to_new_window(self):
+        self.wait.until(EC.new_window_is_opened)
+        windows = self.driver.window_handles
+        print(f'All windows: {windows}')
+        self.driver.switch_to.window(windows[1])
+        print(f'Switched to new window => {windows[1]}')
+
+    def switch_to_newly_opened_window(self):
+        self.wait.until(EC.new_window_is_opened)
+        windows = self.driver.window_handles
+        print(f'All windows: {windows}')
+        self.driver.switch_to.window(windows[1])
+        print(f'Switched to new window => {windows[1]}')
+
+    def switch_to_window_by_id(self, window_id):
+        self.driver.switch_to.window(window_id)
+        print(f'Switched to new window => {window_id}')
+
+    def close(self):
+        self.driver.close()
 
     def wait_until_clickable(self, *locator):
         self.wait.until(
