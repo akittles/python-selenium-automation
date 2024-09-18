b665f6d01074d3dd97070127816e903cf6cc0484
Anthony Kittles <akittlesak4@gmail.com>
2024-09-18 14:01:52 -0400

    Added page

---
+++ pages/terms_conditions_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+from pages.base_page import Page
+
+
+class TermsConditionsPage(Page):
+
+    def verify_tc_url(self):
+        self.verify_partial_url('/terms-conditions')
